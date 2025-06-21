178f3befe782694db93716e70f8e859e0af86131
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-06-21 00:58:52 +0000

    File path

--- Retail-Demand-Forecasting/scripts/data_preprocessing.py
+++ Retail-Demand-Forecasting/scripts/data_preprocessing.py
@@ -1,2 +1,7 @@
 import pandas as pd
 import os
+
+# 📁 File paths
+RAW_DATA_PATH = '/workspaces/Intermediate-Data-Science-Projects/Retail-Demand-Forecasting/dataset/E-commerce Shopping Dataset.csv'
+PROCESSED_DATA_PATH = '/workspaces/Intermediate-Data-Science-Projects/Retail-Demand-Forecasting/processed'
+
