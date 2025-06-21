2607bbcea0e27a1661be83b92b81131b1857d816
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-06-21 01:09:34 +0000

    load data

--- Retail-Demand-Forecasting/scripts/data_preprocessing.py
+++ Retail-Demand-Forecasting/scripts/data_preprocessing.py
@@ -5,3 +5,7 @@ import os
 RAW_DATA_PATH = '/workspaces/Intermediate-Data-Science-Projects/Retail-Demand-Forecasting/dataset/E-commerce Shopping Dataset.csv'
 PROCESSED_DATA_PATH = '/workspaces/Intermediate-Data-Science-Projects/Retail-Demand-Forecasting/processed'
 
+def load_data(path):
+    """Load raw dataset from CSV."""
+    return pd.read_csv(RAW_DATA_PATH)
+
